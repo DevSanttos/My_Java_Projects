2a0a13c17b7c71cc1edead645eece89b2c9af55c
Jonathan Rezende dos Santos <161469965+DevSanttos@users.noreply.github.com>
2025-03-13 15:57:30 -0300

    adicionando um teste de interface gráfica

---
+++ GestaoTarefasEquipe/src/br/com/oliveira/gestao/view/testeDemo.java
@@ -0,0 +1,12 @@
+package br.com.oliveira.gestao.view;
+
+import javax.swing.*;
+
+public class testeDemo {
+    private JPanel telaPrincipal;
+    private JTextField campoNome;
+    private JButton botaoLogin;
+    private JTextField campoSenha;
+    private JLabel senha;
+    private JLabel nome;
+}
